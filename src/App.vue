a1ae21f9989f0c843b39cc725b8d683de2968579
Deividas <deivis124@gmail.com>
2020-09-19 03:57:54 +0300

    Creates Quote Component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <template>
-  <div class="container">
-    <h1>Hallo</h1>
+  <div>
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-4">Wonderfull Quotes</h1>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row bg-primary text-light">
+        <app-quote></app-quote>
+        <app-quote></app-quote>
+        <app-quote></app-quote>
+        <app-quote></app-quote>
+        <app-quote></app-quote>
+        <app-quote></app-quote>
+        <app-quote></app-quote>
+        <app-quote></app-quote>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import appQuote from "./components/Quote.vue";
+
+export default {
+  components: {
+    appQuote,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="col-3">
+    <h5>I am singular Quote</h5>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
